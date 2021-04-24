492bd1078a2c510971aa5f81a1b98ab7ed40c92b
sms <creepercannyon@gmail.com>
2021-04-24 11:27:46 -0700

    Implemented onEnable/Disable/Reload for python scripts.

--- Python/src/main/java/uwu/smsgamer/serverscripter/python/scripts/PyScript.java
+++ Python/src/main/java/uwu/smsgamer/serverscripter/python/scripts/PyScript.java
@@ -1,12 +1,17 @@
 package uwu.smsgamer.serverscripter.python.scripts;
 
+import org.python.core.*;
 import org.python.util.PythonInterpreter;
 import uwu.smsgamer.serverscripter.scripts.Script;
 
 import java.io.*;
+import java.util.*;
 
 public class PyScript extends Script {
     private PythonInterpreter interpreter;
+    public List<PyFunction> enableFuns = new ArrayList<>();
+    public List<PyFunction> reloadFuns = new ArrayList<>();
+    public List<PyFunction> disableFuns = new ArrayList<>();
 
     public PyScript(File scriptFile) {
         super(scriptFile);
@@ -26,20 +31,47 @@ public class PyScript extends Script {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        getGoodFuns();
     }
 
     @Override
     public void enable() {
         System.out.println("Enable: " + scriptFile.getName());
+        execAll(this.enableFuns);
     }
 
     @Override
     public void disable() {
         System.out.println("Disable: " + scriptFile.getName());
+        execAll(this.disableFuns);
     }
 
     @Override
     public void reload() {
         System.out.println("Reload: " + scriptFile.getName());
+        execAll(this.reloadFuns);
+    }
+
+    public void getGoodFuns() {
+        getAndSet("onEnable", enableFuns);
+        getAndSet("on_enable", enableFuns);
+        getAndSet("enable", enableFuns);
+        getAndSet("onReload", reloadFuns);
+        getAndSet("on_reload", reloadFuns);
+        getAndSet("reload", reloadFuns);
+        getAndSet("onDisable", disableFuns);
+        getAndSet("on_disable", disableFuns);
+        getAndSet("disable", disableFuns);
+    }
+
+    private void getAndSet(String name, List<PyFunction> list) {
+        PyObject obj = this.interpreter.get(name);
+        if (obj instanceof PyFunction) list.add((PyFunction) obj);
+    }
+
+    public void execAll(List<PyFunction> funs) {
+        for (PyFunction fun : funs) {
+            fun.__call__();
+        }
     }
 }
