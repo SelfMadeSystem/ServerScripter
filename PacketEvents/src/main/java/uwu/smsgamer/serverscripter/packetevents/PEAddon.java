cc9c8fa5ee68fff231861f6a9f8cb9bad36ba1a6
sms <creepercannyon@gmail.com>
2021-04-25 08:47:01 -0700

    Added PacketEvents addon.

---
+++ PacketEvents/src/main/java/uwu/smsgamer/serverscripter/packetevents/PEAddon.java
@@ -0,0 +1,39 @@
+package uwu.smsgamer.serverscripter.packetevents;
+
+import io.github.retrooper.packetevents.PacketEvents;
+import me.godead.lilliputian.DependencyBuilder;
+import uwu.smsgamer.serverscripter.*;
+
+public class PEAddon extends ScriptAddon {
+    public PEAddon() {
+        this.name = "PacketEvents";
+        this.version = "0.1";
+    }
+
+    @Override
+    public void loadDependencies(DependencyBuilder builder) {
+        // PacketEvents should be already loaded in the server.
+    }
+
+    @Override
+    public void load() {
+        PacketEvents.create(SpigotServerScripter.getInstance());
+        PacketEvents.get().loadAsyncNewThread();
+    }
+
+    @Override
+    public void enable() {
+        PacketEvents.get().registerListener(new ScriptPacketListener());
+        PacketEvents.get().init();
+    }
+
+    @Override
+    public void disable() {
+        PacketEvents.get().terminate();
+    }
+
+    @Override
+    public void reload() {
+
+    }
+}
---
+++ PacketEvents/src/main/java/uwu/smsgamer/serverscripter/packetevents/ScriptPacketListener.java
@@ -0,0 +1,105 @@
+package uwu.smsgamer.serverscripter.packetevents;
+
+import io.github.retrooper.packetevents.event.*;
+import io.github.retrooper.packetevents.event.impl.*;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class ScriptPacketListener extends PacketListenerAbstract {
+    private static ScriptPacketListener instance;
+    // Not the cleanest way of doing it, but it works.
+    public Set<Consumer<PacketStatusReceiveEvent>> packetStatusReceiveFuns = new HashSet<>();
+    public Set<Consumer<PacketStatusSendEvent>> packetStatusSendFuns = new HashSet<>();
+    public Set<Consumer<PacketLoginReceiveEvent>> packetLoginReceiveFuns = new HashSet<>();
+    public Set<Consumer<PacketLoginSendEvent>> packetLoginSendFuns = new HashSet<>();
+    public Set<Consumer<PacketPlayReceiveEvent>> packetPlayReceiveFuns = new HashSet<>();
+    public Set<Consumer<PacketPlaySendEvent>> packetPlaySendFuns = new HashSet<>();
+    public Set<Consumer<PostPacketPlayReceiveEvent>> postPacketPlayReceiveFuns = new HashSet<>();
+    public Set<Consumer<PostPacketPlaySendEvent>> postPacketPlaySendFuns = new HashSet<>();
+    public Set<Consumer<PostPlayerInjectEvent>> postPlayerInjectFuns = new HashSet<>();
+    public Set<Consumer<PlayerInjectEvent>> playerInjectFuns = new HashSet<>();
+    public Set<Consumer<PlayerEjectEvent>> playerEjectFuns = new HashSet<>();
+    public Set<Consumer<PacketEvent>> packetEventExternalFuns = new HashSet<>();
+    public Set<Consumer<PacketHandshakeReceiveEvent>> packetHandshakeReceiveFuns = new HashSet<>();
+
+    public static ScriptPacketListener getInstance() {
+        if (instance == null) instance = new ScriptPacketListener();
+        return instance;
+    }
+
+    @Override
+    public void onPacketStatusReceive(PacketStatusReceiveEvent event) {
+        callAll(packetStatusReceiveFuns, event);
+    }
+
+    @Override
+    public void onPacketStatusSend(PacketStatusSendEvent event) {
+        callAll(packetStatusSendFuns, event);
+    }
+
+    @Override
+    public void onPacketLoginReceive(PacketLoginReceiveEvent event) {
+        callAll(packetLoginReceiveFuns, event);
+    }
+
+    @Override
+    public void onPacketLoginSend(PacketLoginSendEvent event) {
+        callAll(packetLoginSendFuns, event);
+    }
+
+    @Override
+    public void onPacketPlayReceive(PacketPlayReceiveEvent event) {
+        callAll(packetPlayReceiveFuns, event);
+    }
+
+    @Override
+    public void onPacketPlaySend(PacketPlaySendEvent event) {
+        callAll(packetPlaySendFuns, event);
+    }
+
+    @Override
+    public void onPostPacketPlayReceive(PostPacketPlayReceiveEvent event) {
+        callAll(postPacketPlayReceiveFuns, event);
+    }
+
+    @Override
+    public void onPostPacketPlaySend(PostPacketPlaySendEvent event) {
+        callAll(postPacketPlaySendFuns, event);
+    }
+
+    @Override
+    public void onPostPlayerInject(PostPlayerInjectEvent event) {
+        callAll(postPlayerInjectFuns, event);
+    }
+
+    @Override
+    public void onPlayerInject(PlayerInjectEvent event) {
+        callAll(playerInjectFuns, event);
+    }
+
+    @Override
+    public void onPlayerEject(PlayerEjectEvent event) {
+        callAll(playerEjectFuns, event);
+    }
+
+    @Override
+    public void onPacketEventExternal(PacketEvent event) {
+        callAll(packetEventExternalFuns, event);
+    }
+
+    @Override
+    public void onPacketHandshakeReceive(PacketHandshakeReceiveEvent event) {
+        callAll(packetHandshakeReceiveFuns, event);
+    }
+
+    public <E extends PacketEvent>void callAll(Set<Consumer<E>> functions, E event) {
+        for (Consumer<E> function : functions) {
+            try {
+                function.accept(event);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
